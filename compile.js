c756056862cebf4df27b6f566f28a802c45f4921
Heidi Rodriguez <hrod2645@gmail.com>
2018-04-04 12:03:09 -0600

    access bytecode for inbox object

--- compile.js
+++ compile.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFile.Sync(inboxPath, 'utf8');
+const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
