90b44998d8fcc3e2c5f4fa6aadc667b7b41f4406
Heidi Rodriguez <hrod2645@gmail.com>
2018-04-04 11:56:12 -0600

    set inbox.sol path in compile.js

--- compile.js
+++ compile.js
@@ -1,1 +1,10 @@
+'use strict';
 
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFile.Sync(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
