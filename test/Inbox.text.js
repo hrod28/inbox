f26967194a09eda58fe7799abb45bcff30c5c90e
Heidi Rodriguez <hrod2645@gmail.com>
2018-04-08 11:03:21 -0600

    add default message test

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -2,8 +2,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
+
 
 let accounts;
 let inbox;
@@ -20,10 +22,17 @@ beforeEach(async () => {
       arguments: ['Hi there!']
     })
     .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {
+  it('deploys a contract', async () => {
     assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!' );
   });
 });
